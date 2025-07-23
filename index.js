867c824ff8976722be00d8fc149b1c6478124f32
ascuprod <asc.uprod.www@gmail.com>
2025-07-23 19:04:28 +0300

    Hometask

--- index.js
+++ index.js
@@ -1,2 +1,140 @@
 console.log("Hello world!!");
-Console.log("Teacher comment data")
+console.log("Teacher comment data")
+
+
+console.log("Begin8");
+var s=0.0;
+var a =Math.random();
+var b =Math.random();
+console.log((a+b)/2);
+
+console.log("Begin12");
+var d=Math.random();
+var m=Math.random();
+var g=d+m;
+console.log(g);
+var p= d + m + g;
+console.log(p);
+
+console.log("Begin24");
+var aa=Math.random();
+var bb=Math.random();
+var cc=Math.random();
+x=Math.random();
+console.log("A=", aa);
+console.log("B=", bb);
+console.log("C=", cc);
+var temp = cc;
+cc=aa;
+aa=bb;
+bb=temp;
+
+console.log("Begin25");
+var x=Math.random();
+console.log("x=", x);
+console.log("y=", 3*x-6*x-7);
+
+
+console.log("Integer2");
+var xx=Math.floor(Math.random()); 
+var min3=5000;
+var max3=15000;
+var xxxx=(Math.random() * (max3 - min3+1)) + min3;
+var yy=1000;
+console.log(Math.floor(xxxx/yy));
+
+console.log("Integer3");
+var min4=50000;
+var max4=60000;
+var xxxxx=(Math.random() * (max3 - min3+1)) + min3;
+console.log(Math.floor(xxxxx/1024));
+
+console.log("Integer7");
+var min=1;
+var max=99;
+var xxxxxx=(Math.random() * (max - min+1)) + min;
+console.log(xxxxxx);
+const numStr = String(xxxxxx);
+var p1=parseInt(numStr[0], 10);
+var p2=parseInt(numStr[1], 10);
+console.log(Number(p1)+Number(p2));
+console.log(Number(p1)*Number(p2));
+
+console.log("Integer14");
+var min2=100;
+var max2=999;
+var xxxxxy=Math.floor(Math.random() * (max2 - min2+1) + min2);
+console.log(xxxxxy);
+var strrr=xxxxxy.toString();
+var strr3=strrr.slice(0, -1)
+var vvv=2;
+
+console.log(strr3,vvv);
+
+
+console.log("Boolean3");
+var vvvz=23;
+if (vvvz%2==1){
+    console.log("Boolean3");}
+else
+    {console.log("Boolean3");}
+
+console.log("Integer4");
+var vvvzz=4;
+var vvvzzz=1;
+if (vvvzz>2){
+    console.log("A больше 2");}
+else
+    {console.log("A не больше 2");}
+if (vvvzzz<=3){
+    console.log("B меньше или равно 3");}
+else
+    {console.log("B не меньше или равно 3");}
+
+console.log("Integer8");
+var rty=1000
+var rtyu=122
+if (rty%2==0 && rtyu%2==0){
+    console.log("А и В чётные");}
+else
+    {console.log("А и В нечётные");}
+
+console.log("Integer23");
+console.log("Пропустил");
+
+console.log("If3");
+var daa=0;
+if (daa%2==0 && daa!=0){
+    daa=daa+1;
+    console.log(daa);}
+else
+    {if (daa%2==1)
+       {daa=daa-21;
+        console.log(daa);
+    }
+        else {
+            daa=10;
+            console.log(daa);
+        }
+    }
+
+
+console.log("If5");
+
+console.log("If12");
+
+console.log("If18");
+
+
+console.log("Case1");
+
+console.log("Case16");
+
+
+console.log("For2");
+
+console.log("For5");
+
+console.log("For8");
+
+console.log("For10");
